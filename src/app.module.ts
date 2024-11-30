a2e89972902b9fddd2933a0fdbfb91aa1d416285
Dilshan Ramesh <dilshanramesh81@gmail.com>
2024-11-30 22:19:08 +0530

    feat: mongodb configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+//modules
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [CategoryModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_BASE_URL),
+    CategoryModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
